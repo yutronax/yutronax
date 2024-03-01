e44096fc2fbed9b70724c48417f0bdb729e87b7c
yutronax <125690220+yutronax@users.noreply.github.com>
2024-03-01 21:32:55 +0300

    Update adam_asmaca.cpp

--- adam_asmaca.cpp
+++ adam_asmaca.cpp
@@ -1,171 +1,233 @@
+
 #include <iostream>
-#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+
 using namespace std;
+int static s1 = 0;
+int static s2 = 0;
+int static s3 = 0;
+int static s4 = 0;
+int static s5 = 0;
+int static sayaç1 = 5;
+int static skor1 = 0;
+int static skor2 = 0;
+int static sıra ;
+
 int main() {
 	setlocale(LC_ALL, "Turkish");
-	cout << "---/ADAM ASMACAYA (3.0) HOŞGELDİNİZ/--- (tek gerek olan sizinle oynaması için birini bulmak) :)" << endl;
-	cout << "../Lütfen 5 Harfli Kelime Giriniz/.." << endl;
-	char gizle='g';
-	char yharf, yharf1, yharf2, yharf3, yharf4;
-	cin >> yharf >> yharf1 >> yharf2 >> yharf3 >> yharf4;
-	system("cls");
-	cout << endl << "    Tahmin Edilecek Kelime Belirlendi: ";
-	cout << yharf << yharf1 << yharf2 << yharf3 << yharf4 << endl;
-	cout << "Belirlenen Kelimeyi Gizlemek İçin (G)'ye basın" << endl;
-	cin >> gizle;
-	gizle = tolower(gizle);
-	if (gizle=='g')
-	{
-     system("cls");
-	}
-	cout << endl << "//Şimdi Tahmin Zamanı//1. HARFİ GİR VE BAŞLA://" << endl;
-	char tharf,tharf1, tharf2{}, tharf3, tharf4;
-	cin >> tharf;
-	system("cls");
-	if (yharf != tharf)
-	{
-		for (int i = 0; i <= 2; i++)
-		{
-			cout << endl << "../YANLIŞ HARF GİRDİN!!!/.." << endl;
-			cin >> tharf;
+	cout << "ADAM ASMACA 3.0'a HOŞGELDİNİZ!!!"<<endl;
+	cout << "oyunu başlatmak için lütfen (B) basın"<<endl;
+	char başla; for (; ; ) {
+		başla = _getch();
+		if (başla == 'b') {
 			system("cls");
-			if (yharf == tharf)
-			{
-				cout << endl << "..DOĞRU HARF GİRDİN../Kelimenin Şuanki Hali: ";
-				cout << tharf << endl;
-				break;
+			for (int i = 0; i <= 9; i++) {
+				cout << "oyun  başlıyor.";
+				Sleep(250);
+				system("cls"); cout << "oyun  başlıyor..";
+				Sleep(250);
+				system("cls"); cout << "oyun  başlıyor...";
+				Sleep(250);
+				system("cls");
 			}
+			break;
 		}
-		if (yharf != tharf)
-		{
-			cout << "KAYBETTİN";
-			exit(0);
+		else {
+			"oyunu başlatmak için lütfen (B) basın";
+			başla = _getch();
 		}
 	}
-	system("cls");
-	if (yharf == tharf)
-	{
-		cout << endl << "..DOĞRU HARF GİRDİN../Kelimenin Şuanki Hali: ";
-		cout << tharf << endl;
-	}	cin >> tharf1;
-	system("cls");
-	if (yharf1 != tharf1)
-	{
-		for (int i = 0; i <= 2; i++)
-		{
-			cout << endl << "../YANLIŞ HARF GİRDİN!!!/.." << endl;
-			cin >> tharf1;
-			system("cls");
-			cout << "Kelimenin Şuanki Hali:" << tharf << endl;
-			if (yharf1 == tharf1)
-			{
-				cout << endl << "..DOĞRU HARF GİRDİN../Kelimenin Şuanki Hali: ";
-				cout << tharf << tharf1 << endl;
-				break;
-			}
-		}
-		if (yharf1!=tharf1)
-		{
-			cout << "KAYBETTİN";
-			exit(0);
 
-		}
+	char harf1, harf2, harf3, harf4, harf5;
+	cout << " birinci oyuncu için(1) ikinci oyuncu için(2) basın";
+	for (;;) {
+		sıra = _getch();
+		if (sıra == '1' || sıra == '2') {
+			break;
 	}
-	system("cls");
-	if (yharf1 == tharf1)
-	{
-		cout << endl << "..DOĞRU HARF GİRDİN../Kelimenin Şuanki Hali: ";
-		cout << tharf << tharf1 << endl;
+		
+	
+	}
 
-	}
-	cin >> tharf2;
-	system("cls");
-	if (yharf2 != tharf2)
-	{
-		for (int i = 0; i <= 2; i++)
-		{
-			cout << endl << "../YANLIŞ HARF GİRDİN!!!/.." << endl;
-			cin >> tharf2;
+	for (;;) {
+	
+		if (sıra %2==1) {
 			system("cls");
-			cout << "Kelimenin Şuanki Hali:" << tharf<<tharf1 << endl;
-			if (yharf2 == tharf2)
-			{
-				cout << endl << "..DOĞRU HARF GİRDİN../Kelimenin Şuanki Hali: ";
-				cout << tharf << tharf1 << tharf2 << endl;
-				break;
+			cout << "Birinci Oyuncu";
+			cout << endl << "kelimeyi yazınız" << endl;
+			cin >> harf1 >> harf2 >> harf3 >> harf4 >> harf5;
+			cout << endl << "ipucu giriniz" << endl;
+			string ipucu;
+			cin >> ipucu;
+			system("cls");
+			
+			char tahmin;
+			cout << "tahmin vakti" << endl; 
+			cout << "ipucu:" << ipucu << endl;
+			for (;;) {
+				tahmin = _getch();
+				if (tahmin == harf1) {
+					cout << harf1 << " - - - -" << endl;
+					s1++;
+				}
+				if (tahmin == harf2) {
+					cout << "- " << harf2 << " - - -" << endl;
+					s2++;
+				}if (tahmin == harf3) {
+					cout << "- - " << harf3 << " - -" << endl;
+					s3++;
+				}if (tahmin == harf4) {
+					cout << "- - - " << harf4 << " -" << endl;
+					s4++;
+				}if (tahmin == harf5) {
+					cout << "- - - - " << harf5 << endl;
+					s5++;
+				}
+				else if (tahmin != harf1 && tahmin != harf2 && tahmin != harf3 && tahmin != harf4 && tahmin != harf5) {
+					sayaç1--;
+					if (sayaç1 == 4) {
+						cout << endl << endl << " O" << endl;
+					}
+					if (sayaç1 == 3) {
+						cout << endl << endl << " O" << endl << " |" << endl;
+					}if (sayaç1 == 2) {
+						cout << endl << endl << " O" << endl << "-|" << endl;
+					}if (sayaç1 == 1) {
+						cout << endl << endl << " O" << endl << "-|-" << endl;
+					}
+					if (sayaç1 == 0) {
+						cout << endl << endl << " O" << endl << "-|-" << endl << "| |" << endl;
+						cout << endl << "oyun bitti";
+						skor1--;
+						sıra++;
+						break;
+					}
+
+				}
+
+				if (s1>=1&&s2>=1&&s3 >= 1 && s4 >= 1&&s5 >= 1 )
+				{skor1++;
+					system("cls");
+					cout << harf1 << harf2 << harf3 << harf4 << harf5;
+					cout << endl << "tebrikler buldunuz";
+					
+					cout << skor1;
+					s1 = 0;
+					s2 = 0;
+					s3 = 0;
+					s4 = 0;
+					s5 = 0;
+					if (skor1 >= 5) {
+						cout << "Birinci Oyuncu kazandı";
+						exit(0);
+					}
+					
+					
+					sıra++;
+					break;
+				}
+			}
+		}if (sıra%2==0) {
+			system("cls");
+			cout << "İkinci Oyuncu";
+			cout << endl << "kelimeyi yazınız" << endl;
+			cin >> harf1 >> harf2 >> harf3 >> harf4 >> harf5;
+			cout << endl << "ipucu giriniz"<<endl;
+			string ipucu;
+			cin >> ipucu;
+			system("cls");
+			
+			char tahmin;
+			cout << "tahmin vakti" << endl;
+			cout << "ipucu:" << ipucu << endl;
+			for (;;) {
+				tahmin = _getch();
+				if (tahmin == harf1) {
+					cout << harf1 << " - - - -" << endl;
+					s1++;
+				}
+				if (tahmin == harf2) {
+					cout << "- " << harf2 << " - - -" << endl;
+					s2++;
+				}if (tahmin == harf3) {
+					cout << "- - " << harf3 << " - -" << endl;
+					s3++;
+				}if (tahmin == harf4) {
+					cout << "- - - " << harf4 << " -" << endl;
+					s4++;
+				}if (tahmin == harf5) {
+					cout << "- - - - " << harf5 << endl;
+					s5++;
+				}
+				else if (tahmin != harf1 && tahmin != harf2 && tahmin != harf3 && tahmin != harf4 && tahmin != harf5) {
+					sayaç1--;
+					if (sayaç1 == 4) {
+						cout << endl << endl << " O" << endl;
+					}
+					if (sayaç1 == 3) {
+						cout << endl << endl << " O" << endl << " |" << endl;
+					}if (sayaç1 == 2) {
+						cout << endl << endl << " O" << endl << "-|" << endl;
+					}if (sayaç1 == 1) {
+						cout << endl << endl << " O" << endl << "-|-" << endl;
+					}
+					if (sayaç1 == 0) {
+						cout << endl << endl << " O" << endl << "-|-" << endl << "| |" << endl;
+						cout << endl << "oyun bitti";
+						skor2--;
+						sıra++;
+						if (sıra >= 10) {
+							sıra = 0;
+						}
+						break;
+					}
+
+				}
+
+				if (s1 >= 1 && s2 >= 1 && s3 >= 1 && s4 >= 1 && s5 >= 1)
+				{
+					system("cls");
+					cout << harf1 << harf2 << harf3 << harf4 << harf5;
+					cout << endl << "tebrikler buldunuz";
+					skor2++;
+					cout << skor2;
+
+					s1 = 0;
+					s2 = 0;
+					s3 = 0;
+					s4 = 0;
+					s5 = 0;
+					if (skor2 >= 5) {
+						cout <<endl<< "İkinci Oyuncu kazandı";
+						exit(0);
+					}
+					sıra++;
+					if (sıra >= 10) {
+						sıra = 0;
+					}
+					break;
+				}
 			}
 		}
-		if (yharf2 != tharf2)
-		{
-			cout << "KAYBETTİN";
-			exit(0);
-		}
-	}
-	system("cls");
-	if (yharf2 == tharf2)
-	{
-		cout << endl << "..DOĞRU HARF GİRDİN../Kelimenin Şuanki Hali: ";
-		cout << tharf << tharf1 << tharf2 << endl;
-	}
-	cin >> tharf3;
-	system("cls");
-	if (yharf3 != tharf3)
-	{
-		for (int i = 0; i <= 2; i++)
-		{
-			cout << endl << "../YANLIŞ HARF GİRDİN!!!/.." << endl;
-			cin >> tharf3;
+		if (skor1 >= 5 || skor2 >= 5) {
+			char tekrar;
 			system("cls");
-			cout << "Kelimenin Şuanki Hali:" << tharf << tharf1 <<tharf2<< endl;
-			if (yharf3 == tharf3)
+			cout << "tekrar oynamak istiyor musunuz?\n hayır(H)         evet(E)";
+
+			tekrar = _getch();
+			if (tekrar == 'e') {
+				
+			}
+			else if (tekrar == 'h')
 			{
-				cout << endl << "..DOĞRU HARF GİRDİN../Kelimenin Şuanki Hali: ";
-				cout << tharf << tharf1 << tharf2 << tharf3 << endl;
-				break;
+				exit(0);
+			}
+			else if (tekrar != 'e' || tekrar == 'h') {
+				tekrar = _getch();
+
 			}
 		}
-		if (yharf3 != tharf3)
-		{
-			cout << "KAYBETTİN";
-			exit(0);
-		}
 	}
-	system("cls");
-	if (yharf3 == tharf3)
-	{
-		cout << endl << "..DOĞRU HARF GİRDİN../Kelimenin Şuanki Hali: ";
-		cout << tharf << tharf1 << tharf2 << tharf3 << endl;
-	}
-	cin >> tharf4;
-	system("cls");
-	if (yharf4 != tharf4)
-	{
-		for (int i = 0; i <= 2; i++)
-		{
-			cout << endl << "../YANLIŞ HARF GİRDİN!!!/.." << endl;
-			cin >> tharf4;
-			system("cls");
-			cout << "Kelimenin Şuanki Hali:" << tharf << tharf1 << tharf2 << tharf3 << endl;
-			if (yharf4 == tharf4)
-			{
-				cout << endl << "..DOĞRU HARF GİRDİN../Kelimenin Şuanki Hali: ";
-				cout << tharf << tharf1 << tharf2 << tharf3 << tharf4 << endl;
-				break;
-			}
-		}
-		if (yharf4 != tharf4)
-		{
-			cout << "KAYBETTİN";
-			exit(0);
-		}
-	}
-	system("cls");
-	if (yharf4 == tharf4)
-	{
-		cout << endl << "..DOĞRU HARF GİRDİN VE KELİME TAMAMLANDI../Kelime: ";
-		cout << tharf << tharf1 << tharf2 << tharf3 << tharf4 << endl;
-	}
-	cout << "TEBRİKLER KELİMEYİ BULDUNUZ" << endl;
-	cout << "..//YAPIMCILAR: Yusuf ÇINAR & Eren HELVACI//.." << endl;
 	return 0;
 }
